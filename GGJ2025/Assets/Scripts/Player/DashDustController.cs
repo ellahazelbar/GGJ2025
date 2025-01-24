fbfe2adcd682eee3d3e6630f485824e474598ae5
NeoThePotato <neorayb@gmail.com>
2025-01-25 00:51:08 +0200

    Aim dash dust against movement direction.

--- GGJ2025/Assets/Scripts/Player/DashDustController.cs
+++ GGJ2025/Assets/Scripts/Player/DashDustController.cs
@@ -12,6 +12,10 @@ namespace Player
 
 		private void OnDisable() => _dash.Dashed -= OnDash;
 
-		private void OnDash() => _ps.Play();
+		private void OnDash()
+		{
+			_ps.transform.localScale = new(Mathf.Sign(_dash.transform.localPosition.x), 1f, 1f);
+			_ps.Play();
+		}
 	}
 }
