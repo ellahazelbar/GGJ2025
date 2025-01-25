86b8e4d7b6c305a9733818bf1a80fd8ce22a246f
NeoThePotato <neorayb@gmail.com>
2025-01-25 15:37:23 +0200

    dslghdsf

--- GGJ2025/Assets/Scripts/Player/PlayAnimationOnInteract.cs
+++ GGJ2025/Assets/Scripts/Player/PlayAnimationOnInteract.cs
@@ -30,6 +30,7 @@ namespace Player
 
 		private void ShowStartAnimation(Instrument instrument)
 		{
+			instrument.GetComponentInChildren<SpriteRenderer>().gameObject.SetActive(false);
 			_instrumentAnimator.InstrumentMinigame = instrument.Minigame;
 			var definition = _animationDefinition[(int)instrument.Type];
 			_instrumentAnimator.StartAnimation = definition.SetupAnimation;
